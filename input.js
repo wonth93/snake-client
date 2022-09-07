e7eccf9eceafb64a68428c211440ee0578a5d5b8
wonth93 <wongth93@gmail.com>
2022-09-07 04:50:12 +0000

    input module

---
+++ input.js
@@ -0,0 +1,18 @@
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (input) {
+  // \u0003 maps to ctrl+c input
+  if (input === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
